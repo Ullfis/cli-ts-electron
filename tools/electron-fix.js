e9fa651d2d23d5191a12cf4d4119cc6139164c43
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-10-19 12:43:36 +0200

    fix: build prod and stage throw error
    'SyntaxError: Unexpected token name «key», expected punc «;»'

--- tools/electron-fix.js
+++ tools/electron-fix.js
@@ -1,11 +1,11 @@
 // https://github.com/aurelia/cli/issues/217
 define("electron", ['exports'], function (exports) {
   if (window.nodeRequire) {
-    const electron = window.nodeRequire("electron");
+    var electron = window.nodeRequire("electron");
 
     exports["default"] = electron;
 
-    for (let key in electron) {
+    for (var key in electron) {
       exports[key] = electron[key];
     }
   }
