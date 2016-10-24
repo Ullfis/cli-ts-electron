0fa8e4d353149da0e40c698cb71002577f1899b2
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-10-24 15:42:59 +0200

    bluebird and fetch is included in chromium
    - sometimes $ is undefined. moved jquery to vendor-bundle
    - add index.js to aurelia-materialize-bridge resources

--- src/main.ts
+++ src/main.ts
@@ -4,32 +4,34 @@ import * as Backend from 'i18next-xhr-backend';
 
 //Configure Bluebird Promises.
 //Note: You may want to use environment-specific configuration.
-(<any>Promise).config({
-  warnings: {
-    wForgottenReturn: false
-  }
-});
+// (<any>Promise).config({
+//   warnings: {
+//     wForgottenReturn: false
+//   }
+// });
 
 export async function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature('resources')
-    .plugin('aurelia-i18n', instance => {
-      instance.i18next.use(Backend);
-      return instance.setup({
-        ns: ['translation', 'admin'],
-        defaultNs: 'translation',
-        backend: {
-          loadPath: 'locales/{{lng}}/{{ns}}.json',
-        },
-        lng : 'en',
-        attributes : ['t','i18n'],
-        fallbackLng : 'en',
-        debug : false
-      });
-    })    
-    .plugin('aurelia-validation')
-    .plugin('aurelia-materialize-bridge', b => b.useAll());
+    .feature('resources');
+
+  aurelia.use.plugin('aurelia-i18n', instance => {
+    instance.i18next.use(Backend);
+    return instance.setup({
+      ns: ['translation', 'admin'],
+      defaultNs: 'translation',
+      backend: {
+        loadPath: 'locales/{{lng}}/{{ns}}.json',
+      },
+      lng : 'en',
+      attributes : ['t','i18n'],
+      fallbackLng : 'en',
+      debug : false
+    });
+  });
+
+  aurelia.use.plugin('aurelia-validation');
+  aurelia.use.plugin('aurelia-materialize-bridge', b => b.useAll());
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
--- src/users.ts
+++ src/users.ts
@@ -3,7 +3,7 @@ import {HttpClient} from 'aurelia-fetch-client';
 
 // polyfill fetch client conditionally
 // const fetch = !self.fetch ? System.import('isomorphic-fetch') : Promise.resolve(self.fetch);
-const fetch = !self.fetch ? require('isomorphic-fetch') : Promise.resolve(self.fetch);
+// const fetch = !self.fetch ? require('isomorphic-fetch') : Promise.resolve(self.fetch);
 
 interface IUser {
   avatar_url: string;
@@ -20,7 +20,7 @@ export class Users {
 
   async activate(): Promise<void> {
     // ensure fetch is polyfilled before we create the http client
-    await fetch;
+    // await fetch;
     const http = this.http = this.getHttpClient();
 
     http.configure(config => {
@@ -31,6 +31,5 @@ export class Users {
 
     const response = await http.fetch('users');
     this.users = await response.json();
-
   }
 }
